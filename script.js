533020bc11675a18a990d2904a6fb5c98536351b
Saheer Babu <sheerbabu549@gmail.com>
2024-07-07 16:42:08 +0530

    Bro forgot the '//' slash on the commend

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const numArr=[12, 23, 50, 10, 5, 90];
 const sortArr=numArr.sort((a,b)=> b-a)
 console.log(sortArr);
 // hello this is the git baranch from feature
+commend slash forgotted my another developer
